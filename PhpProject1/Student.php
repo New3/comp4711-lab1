9e8c4e90fec9e556689ac8cadd8f3ca7e26887c6
kwanc <kwanc@142.232.174.194>
2016-09-11 00:16:05 -0700

    add @var and branch on student.php class

--- PhpProject1/Student.php
+++ PhpProject1/Student.php
@@ -30,17 +30,20 @@ class Student {
    function average(){
        $total = 0;
        foreach ($this->grades as $value)
-           $total += $value;
-       return $total / count($this->grades);
+        /* @var $value type */ {
+            $total += $value;
+        }
+        return $total / count($this->grades);
    }
    
    function toString(){
        $result = $this->first_name . '' .
                $this->surname;
        $result .= '('.$this->average().")\n";
-       foreach($this->email as $which=>$what)
-           $result .=$which . ': '. $what. "\n";
-       $result .= "\n";
+       foreach ($this->email as $which => $what) {
+            $result .=$which . ': ' . $what . "\n";
+        }
+        $result .= "\n";
        return '<pre>'.$result.'</pre>';
    }
 }
